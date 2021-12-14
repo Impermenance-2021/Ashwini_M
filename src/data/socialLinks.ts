13ecdd4247e92150f5d48677a1b0881adfc5a846
ASHWINI MATHUR <88894984+Impermenance-2021@users.noreply.github.com>
2021-12-14 21:14:37 +0530

    Update socialLinks.ts

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -4,18 +4,6 @@ import type { SocialLink } from '../types/SocialLink';
 
 export const socialLinks: SocialLink[] = [
   {
-    type: socialLinkTypes.linkedIn,
-    url: 'https://www.linkedin.com/in/ashwini-mathur-596a3758/',
-    secondary: false,
-    caption: 'Ashwini Mathur on LinkedIn',
-  },
-  {
-    type: socialLinkTypes.gitHub,
-    url: 'https://github.com/Impermenance-2021',
-    secondary: false,
-    caption: 'Ashwini Mathur on GitHub',
-  },
-  {
     type: socialLinkTypes.facebook,
     url: 'https://www.facebook.com/ashwini.mathur.3',
     secondary: true,
@@ -27,5 +15,16 @@ export const socialLinks: SocialLink[] = [
     secondary: true,
     caption: 'Ashwini Mathur on Instagram',
   },
-  
+  {
+    type: socialLinkTypes.linkedIn,
+    url: 'https://www.linkedin.com/in/ashwini-mathur-596a3758/',
+    secondary: false,
+    caption: 'Ashwini Mathur on LinkedIn',
+  },
+  {
+    type: socialLinkTypes.gitHub,
+    url: 'https://github.com/Impermenance-2021',
+    secondary: false,
+    caption: 'Ashwini Mathur on GitHub',
+  },
 ];
