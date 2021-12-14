da3a0d613084995b62f18f7be5d7056f2f3298d3
ASHWINI MATHUR <88894984+Impermenance-2021@users.noreply.github.com>
2021-12-14 21:12:34 +0530

    Update socialLinks.ts

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -5,56 +5,27 @@ import type { SocialLink } from '../types/SocialLink';
 export const socialLinks: SocialLink[] = [
   {
     type: socialLinkTypes.linkedIn,
-    url: 'https://www.linkedin.com/in/trekhleb/',
+    url: 'https://www.linkedin.com/in/ashwini-mathur-596a3758/',
     secondary: false,
-    caption: 'Oleksii Trekhleb on LinkedIn',
+    caption: 'Ashwini Mathur on LinkedIn',
   },
   {
     type: socialLinkTypes.gitHub,
-    url: 'https://github.com/trekhleb',
+    url: 'https://github.com/Impermenance-2021',
     secondary: false,
-    caption: 'Oleksii Trekhleb on GitHub',
-  },
-  {
-    type: socialLinkTypes.twitter,
-    url: 'https://twitter.com/Trekhleb',
-    secondary: false,
-    caption: 'Oleksii Trekhleb on Twitter',
+    caption: 'Ashwini Mathur on GitHub',
   },
   {
     type: socialLinkTypes.facebook,
-    url: 'https://www.facebook.com/oleksii.trekhleb',
+    url: 'https://www.facebook.com/ashwini.mathur.3',
     secondary: true,
-    caption: 'Oleksii Trekhleb on Facebook',
+    caption: 'Ashwini Mathur on Facebook',
   },
   {
     type: socialLinkTypes.instagram,
-    url: 'https://www.instagram.com/oleksii_trekhleb',
+    url: 'https://www.instagram.com/91_ashu/',
     secondary: true,
-    caption: 'Oleksii Trekhleb on Instagram',
+    caption: 'Ashwini Mathur on Instagram',
   },
-  {
-    type: socialLinkTypes.medium,
-    url: 'https://medium.com/@trekhleb',
-    secondary: true,
-    caption: 'Oleksii Trekhleb on Medium',
-  },
-  {
-    type: socialLinkTypes.devTo,
-    url: 'https://dev.to/trekhleb',
-    secondary: true,
-    caption: 'Oleksii Trekhleb on DevTo',
-  },
-  {
-    type: socialLinkTypes.stackOverflow,
-    url: 'https://stackoverflow.com/users/7794070/oleksii-trekhleb',
-    secondary: true,
-    caption: 'Oleksii Trekhleb on StackOverflow',
-  },
-  {
-    type: socialLinkTypes.youTube,
-    url: 'https://www.youtube.com/c/OleksiiTrekhlebs',
-    secondary: true,
-    caption: 'Oleksii Trekhleb on YouTube',
-  },
+  
 ];
