de76a2d811b838679c6c9cd25585d6ad6a5b4d1d
ASHWINI MATHUR <88894984+Impermenance-2021@users.noreply.github.com>
2022-01-03 13:25:34 +0530

    Update Greeting.tsx

--- src/components/shared/Greeting.tsx
+++ src/components/shared/Greeting.tsx
@@ -28,14 +28,12 @@ const Greeting = (): React.ReactElement => {
     <>
       <p className="font-light">
         Hi there!
-        I&apos;m Oleksii. I work as a full-time software engineer. In my spare time I do
+        I&apos;m Ashwini Mathur. I work as a full-time Educator and Assistant Teaching Professor. In my spare time I do
         open-sourcing <i>(normally it is about 1-2 hours in the morning before the work)</i>.
         Most of my open-source {projectsLinkElement} are focused
         on one thing &ndash; <i>to help people learn</i>. You may use these projects to
-        learn about algorithms in JavaScript and get prepared for technical interviews, or
-        to learn Python syntax and start experimenting with machine learning algorithms
-        and math behind them, etc. I also write {blogLinkElement} about life, web-development
-        and machine learning.
+        learn about Full Stack Web-Devolepment and other useful learning snippets, etc. I also write {blogLinkElement} about life, web-development
+        R programming, Linux and machine learning etc...
       </p>
     </>
   );
